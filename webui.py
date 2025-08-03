e59fd6030c53cbc5607eff64156f1040b74522cd
linyq <linyqemail@163.com>
2025-08-03 17:12:07 +0800

    refactor: 移除视频字幕合并功能及相关代码
    清理不再使用的视频字幕合并功能，包括删除合并设置组件、合并工具函数和相关的国际化文本

--- webui.py
+++ webui.py
@@ -4,7 +4,7 @@ import sys
 from loguru import logger
 from app.config import config
 from webui.components import basic_settings, video_settings, audio_settings, subtitle_settings, script_settings, \
-    review_settings, merge_settings, system_settings
+    review_settings, system_settings
 # from webui.utils import cache, file_utils
 from app.utils import utils
 from app.utils import ffmpeg_utils
@@ -221,8 +221,6 @@ def main():
     # 首先渲染不依赖PyTorch的UI部分
     # 渲染基础设置面板
     basic_settings.render_basic_settings(tr)
-    # 渲染合并设置
-    merge_settings.render_merge_settings(tr)
 
     # 渲染主面板
     panel = st.columns(3)
